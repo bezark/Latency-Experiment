45f08f5d3d09e87d6db8a1ee7422a5fb010238b1
bezark <jdb459@nyu.edu>
2024-09-10 17:54:37 -0400

    Update ball.gd comments

--- latencyexperiment/Breakout/ball.gd
+++ latencyexperiment/Breakout/ball.gd
@@ -6,4 +6,4 @@ func _on_body_entered(body: Node) -> void:
 
 	if body.is_in_group("player"):
 		$"../EngineControls".randomize_sliders() # Very Chaotic
-		#$"../EngineControls".random_walk_sliders() # Very Chaotic
+		#$"../EngineControls".random_walk_sliders() # less Chaotic
