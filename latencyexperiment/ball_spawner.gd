a13c4c9171a040cc41fcfeacd3e8ed39f06665b2
Ari Gass <45432806+gasm0ney@users.noreply.github.com>
2024-09-10 16:47:18 -0400

    infinite balls based on frames drawn
    currently spawns an exponential number of balls per frame drawn

--- latencyexperiment/ball_spawner.gd
+++ latencyexperiment/ball_spawner.gd
@@ -1,12 +1,12 @@
 extends Node3D
-@export var number_of_marbles = 20
+@export var number_of_marbles = 0
 const BALL = preload("res://ball.tscn")
 
 
-func _ready():
-	for x in number_of_marbles:
+func _process(delta: float) -> void:
+	for x in Engine.get_frames_drawn():
 		var new_ball = BALL.instantiate()
-		new_ball.position.y = x*0.5
-		new_ball.position.x = randf_range(-0.1, 0.1)
+		#new_ball.position.y = Engine.get_frames_drawn()
+		#new_ball.position.x = randf_range(-0.1, 0.1)
 		add_child(new_ball)
 		
