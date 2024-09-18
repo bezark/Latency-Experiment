ac97bd020f552334f8ad53feaf38a004c60f1bcc
bezark <jdb459@nyu.edu>
2024-09-18 12:38:39 -0400

    3rd Person Camera and other tweaks
    - Added simple 3rd person camera. Hit esc to retrieve your mouse. Added code to Game Over scene to retrieve your mouse. The camera uses a spring arm to shoot a raycast out and if it detects a collision it adjusts the position of it's child (the camera). This means the camera wont go through walls
    - Speaking of collisions, the ball and bricks were not actually responding to collisions with the player??? They were preventing clipping but they weren't actually transferring velocity? I'm a little unclear as to why but by messing with the collision layers and masks we now have much more realistic collisions. I followed [this vide](https://www.youtube.com/watch?v=s58HA3lySpU) which did not particularly explain their logic  but you can learn a littler more [here](https://docs.godotengine.org/en/stable/tutorials/physics/physics_introduction.html#doc-physics-introduction-collision-layers-and-masks)
    - in creating the camera controller i thought i needed to adjust the player. Instead of scaling the player, now i have scaled the mesh and have replaced the collision shape with a box. I could probably revert some of this if it's undesired
    - Shrank the height of the walls

--- latencyexperiment/Player.gd
+++ latencyexperiment/Player.gd
@@ -4,6 +4,11 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+@onready var pivot = $CameraOrigin
+@export var sensitivity = 0.5
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -27,3 +32,13 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+	#Release mouse
+	if Input.is_action_just_pressed("Escape"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x *sensitivity))
+		pivot.rotate_x(deg_to_rad(-event.relative.y *sensitivity))
+		pivot.rotation.x = clamp(pivot.rotation.x, deg_to_rad(-90), deg_to_rad(45))
---
+++ latencyexperiment/game_over.gd
@@ -0,0 +1,6 @@
+extends Control
+
+
+func _ready():
+	#release the mouse
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
